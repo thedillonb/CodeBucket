a3fe05f7909d37546615d5c6e48f6dc21d5134d5
Dillon Buchanan <thedillonb@gmail.com>
2012-10-15 19:35:01 -0400

    More changes
    Conflicts:
    
    	BitbucketBrowser/Main.cs
    	BitbucketBrowser/UI/Controllers/Source/SourceInfoController.cs

--- CodeFramework/Utils/Util.cs
+++ CodeFramework/Utils/Util.cs
@@ -184,6 +184,25 @@ namespace MonoTouch
                         * UIScreen.MainScreen.Scale >= 1136;
             }     
         }
+
+        public static Tuple<int, int> iOSVersion
+        {
+            get
+            {
+                try
+                {
+                    var version = UIDevice.CurrentDevice.SystemVersion.Split('.');
+                    var major = Int32.Parse(version[0]);
+                    var minor = Int32.Parse(version[1]);
+                    return new Tuple<int, int>(major, minor);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("When attempting to get version: " + e.Message);
+                    return new Tuple<int, int>(5, 0);
+                }
+            }
+        }
     }
 }
 
