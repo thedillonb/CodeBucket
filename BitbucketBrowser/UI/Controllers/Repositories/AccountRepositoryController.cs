b9b30e667e5aa2c533da37fbd71a7a70abf6e17c
Dillon Buchanan <thedillonb@gmail.com>
2012-10-17 02:33:26 -0400

    Will now save the account repository selection
    Conflicts:
    
    	BitbucketBrowser/UI/Controllers/Repositories/AccountRepositoryController.cs

--- BitbucketBrowser/UI/Controllers/Repositories/AccountRepositoryController.cs
+++ BitbucketBrowser/UI/Controllers/Repositories/AccountRepositoryController.cs
@@ -1,3 +1,4 @@
+using System;
 using MonoTouch.UIKit;
 using MonoTouch.Dialog;
 using System.Linq;
@@ -8,93 +9,106 @@ namespace BitbucketBrowser.UI.Controllers.Repositories
 {
     public class AccountRepositoryController : RepositoryController
     {
-        private readonly UISegmentedControl _segment = new UISegmentedControl(new[] { "Owned", "Following" });
-
+        private static string SavedSelection = "REPO_SELECTION";
+        private UISegmentedControl _segment = new UISegmentedControl(new [] { "Owned", "Following" });
+        
         public AccountRepositoryController(string username)
             : base(username)
         {
         }
-
-        protected override void OnRefresh()
+        
+        protected override void OnRefresh ()
         {
             if (Root != null)
-                InvokeOnMainThread(() => Root.Clear());
-
+                InvokeOnMainThread(delegate { Root.Clear(); });
+            
             if (Model.Count == 0)
                 return;
-
+            
             var selected = 0;
             InvokeOnMainThread(() => { selected = _segment.SelectedSegment; });
-
+            
             var sec = new Section();
-            Model.ForEach(x =>
-            {
-                var sse = new RepositoryElement(x) { ShowOwner = selected != 0 };
+            Model.ForEach(x => {
+                RepositoryElement sse = new RepositoryElement(x) { ShowOwner = selected != 0 };
                 sse.Tapped += () => NavigationController.PushViewController(new RepositoryInfoController(x), true);
                 sec.Add(sse);
             });
-
+            
             //Sort them by name
             sec.Elements = sec.Elements.OrderBy(x => ((RepositoryElement)x).Model.Name).ToList();
 
-            InvokeOnMainThread(delegate
-            {
+            InvokeOnMainThread(delegate {
                 Root = new RootElement(Title) { sec };
             });
         }
-
-        protected override List<RepositoryDetailedModel> OnUpdate(bool forced)
+        
+        protected override List<RepositoryDetailedModel> OnUpdate (bool forced)
         {
             var selected = 0;
             InvokeOnMainThread(() => { selected = _segment.SelectedSegment; });
-
+            
             if (selected == 0)
                 return Application.Client.Users[Username].GetInfo(forced).Repositories;
-            return selected == 1 ? Application.Client.Account.GetRepositories(forced) : new List<RepositoryDetailedModel>();
+            else if (selected == 1)
+                return Application.Client.Account.GetRepositories(forced);
+            else
+                return new List<RepositoryDetailedModel>();
         }
-
-
+        
+        
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-
+            
             _segment.ControlStyle = UISegmentedControlStyle.Bar;
             _segment.SelectedSegment = 0;
-
+            
             //Fucking bug in the divider
-            BeginInvokeOnMainThread(delegate
-            {
+            BeginInvokeOnMainThread(delegate {
                 _segment.SelectedSegment = 1;
                 _segment.SelectedSegment = 0;
+                _segment.SelectedSegment = MonoTouch.Utilities.Defaults.IntForKey(SavedSelection);
+                SetTitle();
                 _segment.ValueChanged += (sender, e) => ChangeSegment();
             });
-
+            
             Title = "Owned";
-
+            
             //The bottom bar
-            var btn = new UIBarButtonItem(_segment) { Width = View.Frame.Width - 10f };
-            ToolbarItems = new[] { new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace), btn, new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace) };
+            var btn = new UIBarButtonItem(_segment);
+            btn.Width = View.Frame.Width - 10f;
+            ToolbarItems = new [] { new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace), btn, new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace) };
         }
-
+        
         public override void ViewWillAppear(bool animated)
         {
-            NavigationController.SetToolbarHidden(IsSearching, animated);
+            NavigationController.SetToolbarHidden(isSearching, animated);
             base.ViewWillAppear(animated);
         }
-
+        
         public override void ViewWillDisappear(bool animated)
         {
             base.ViewWillDisappear(animated);
             NavigationController.SetToolbarHidden(true, animated);
         }
-
+        
         private void ChangeSegment()
         {
-            Root.Clear();
+            Root.Clear(); 
             TableView.TableFooterView.Hidden = true;
             Model = null;
             Refresh();
+            
+            SetTitle();
 
+            //Remember the default value!
+            MonoTouch.Utilities.Defaults.SetInt(_segment.SelectedSegment, SavedSelection);
+            MonoTouch.Utilities.Defaults.Synchronize();
+        }
+
+        private void SetTitle()
+        {
             if (_segment.SelectedSegment == 0)
                 Title = "Owned";
             else if (_segment.SelectedSegment == 1)
@@ -102,13 +116,13 @@ namespace BitbucketBrowser.UI.Controllers.Repositories
             else if (_segment.SelectedSegment == 2)
                 Title = "Viewed";
         }
-
+        
         protected override void SearchStart()
         {
             NavigationController.SetToolbarHidden(true, false);
             base.SearchStart();
         }
-
+        
         protected override void SearchEnd()
         {
             NavigationController.SetToolbarHidden(false, false);
